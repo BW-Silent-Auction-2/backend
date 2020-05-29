b5ffb4668f40517316e03cd91554a6c04b110a60
Devin <hoangong71@gmail.com>
2020-05-28 17:23:03 -0700

    pg

--- data/config.js
+++ data/config.js
@@ -1,8 +1,11 @@
 
 const knex = require('knex');
 
+// We can actually just skip DB_ENV and just set it knex(knexfile[process.env.NODE_ENV]) since
+// Heroku set it as production as default
+
 const env = process.env.DB_ENV || 'development'
 
 const knexfile = require("../knexfile")
 
-module.exports = knex(knexfile[process.env.NODE_ENV])
+module.exports = knex(knexfile[env])
